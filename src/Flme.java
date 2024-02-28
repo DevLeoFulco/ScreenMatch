49ac0f0f81fb90285fb1ff6a59e802875008a72b
DevLeoFulco <devleofulco@gmail.com>
2024-02-28 17:32:07 -0300

    First Commit

---
+++ src/Flme.java
@@ -0,0 +1,10 @@
+public class Flme {
+    String titulo;
+    String descricao;
+    int anoLancamento;
+    boolean incluidoNoPlano;
+    String genero;
+    int classificacaoIndicativa;
+    double avaliacaoIbm;
+    int duracaoEmMinutos;
+}
